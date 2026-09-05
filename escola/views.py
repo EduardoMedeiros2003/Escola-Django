7979c9f453c0687b2359839bcf6ceba8169c692a
Eduardo Umbelino <umbelino.eduardo2003@gmail.com>
2026-09-04 21:15:53 -0300

    feat: configure global API authentication
    Configura a autenticação e as permissões padrão do Django REST Framework.
    
    Adiciona BasicAuthentication como método de autenticação padrão.
    Define IsAuthenticated como permissão padrão da API.
    Passa a exigir autenticação nos endpoints do projeto por padrão.
    Centraliza a configuração de segurança no settings.py.

--- escola/views.py
+++ escola/views.py
@@ -5,34 +5,24 @@ from rest_framework.authentication import BaseAuthentication
 from rest_framework.permissions import IsAuthenticated
 
 class EstudanteViewSet(viewsets.ModelViewSet):
-    authentication_classes = [BaseAuthentication]
-    permission_classes =[IsAuthenticated]
     queryset = Estudante.objects.all()
     serializer_class = EstudanteSerializer
 
 class CursoViewSet(viewsets.ModelViewSet):
-    authentication_classes = [BaseAuthentication]
-    permission_classes =[IsAuthenticated]
     queryset = Curso.objects.all()
     serializer_class = CursoSerializer
 
 class MatriculaViewSet(viewsets.ModelViewSet):
-    authentication_classes = [BaseAuthentication]
-    permission_classes =[IsAuthenticated]
     queryset = Matricula.objects.all()
     serializer_class = MatriculaSerializer
 
 class ListaMatriculaEstudante(generics.ListAPIView):
-    authentication_classes = [BaseAuthentication]
-    permission_classes =[IsAuthenticated]
     def get_queryset(self):
         queryset = Matricula.objects.filter(estudante_id=self.kwargs['pk'])
         return queryset
     serializer_class = ListaMatriculasEstudanteSerializer
 
 class ListaMatriculaCurso(generics.ListAPIView):
-    authentication_classes = [BaseAuthentication]
-    permission_classes =[IsAuthenticated]
     def get_queryset(self):
            queryset = Matricula.objects.filter(Curso_id=self.kwargs['pk'])
            return queryset
